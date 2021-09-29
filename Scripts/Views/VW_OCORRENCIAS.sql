caec6e97112c67bc4f95826cf39cadec4d7cae72
luizcamini <luizcamini@outlook.com>
2021-09-28 22:51:02 -0300

    Atualizacao vw_ocorrencias

--- Scripts/Views/VW_OCORRENCIAS.sql
+++ Scripts/Views/VW_OCORRENCIAS.sql
@@ -1,13 +1,15 @@
 CREATE
-VIEW VW_OCORRENCIAS AS
-SELECT doc.DocId
+OR ALTER VIEW VW_OCORRENCIAS AS
+SELECT distinct doc.DocId
      , doc.NumeroDoc
      , FORMAT(doc.DataEmissao,'dd/MM/yyyy') AS DataEmissaoDocumento
      , doc.TipoDocumentoId
+     , cid.EstadoId + cid.CidadeId AS CodigoIBGECidadeRem
      , dr.CEP AS CEPRemetente
      , dr.Cidade AS CidadeRemetente
      , dr.UF AS EstadoRemetente
      , UF.NOMEREGIAO AS RegiaoRemetente
+     , cid2.EstadoId + cid2.CidadeId AS CodigoIBGECidadeDest
      , dd.CEP AS CEPDestinatario
      , dd.Cidade AS CidadeDestinatario
      , dd.UF AS EstadoDestinatario
@@ -21,34 +23,28 @@ SELECT doc.DocId
      , dr.CpfCnpj AS Remetente
      , dd.CpfCnpj AS Destinatario
      , FORMAT(dh.DataCadastro,'dd/MM/yyyy') AS DataIsercao
-  FROM Documentos doc INNER JOIN
-       SitraWebNew.dbo.Filiais fil ON fil.FilialId = doc.FilialId INNER JOIN
-       SitraWebNew.dbo.TipoDocumento td ON td.TipoDocumentoId = doc.TipoDocumentoId INNER JOIN
-       DocumentoHistorico dh ON dh.DocId = doc.DocId
+  FROM Documentos doc 
+  INNER JOIN  SitraWebNew.dbo.Filiais fil ON fil.FilialId = doc.FilialId 
+  INNER JOIN  SitraWebNew.dbo.TipoDocumento td ON td.TipoDocumentoId = doc.TipoDocumentoId 
+  INNER JOIN  DocumentoHistorico dh ON dh.DocId = doc.DocId
    AND dh.OcorrenciaId IS NOT NULL
-   AND dh.OcorrenciaId != 0 INNER JOIN
-       SitraWebNew.dbo.Usuarios cadUser ON cadUser.UsuarioId = dh.UsuarioIdHistorico INNER JOIN
-       DocRemetente dr ON dr.ClienteId = doc.RemetenteId
-   AND dr.DocId = doc.DocId INNER JOIN
-       DocDestinatario dd ON dd.ClienteId = doc.DestinatarioId
-   AND dd.DocId = doc.DocId INNER JOIN
-       DocTomador dt ON dt.ClienteId = doc.TomadorId
-   AND dt.DocId = doc.DocId LEFT JOIN
-       Ocorrencias oc ON oc.OcorrenciaId = dh.OcorrenciaId
-   AND oc.OcorrenciaTipo = 'E'
-   AND dh.OcorrenciaCliente = 0 LEFT JOIN
-       OcorClienteEntrega occ ON occ.OcorrenciaId = dh.OcorrenciaId
-   AND occ.ClienteId = dt.ClienteId
-   AND dh.OcorrenciaCliente = 1 LEFT JOIN
-       sitrawebnew.dbo.cep AS CEP ON DR.CEP = CEP.CepId LEFT JOIN
-       sitrawebnew.dbo.cep AS CEP2 ON DD.CEP = CEP.CepId LEFT JOIN
-       sitrawebnew.dbo.Estados AS UF ON DR.UF = UF.UF LEFT JOIN
-       sitrawebnew.dbo.Estados AS UF2 ON DD.UF = UF.UF LEFT JOIN
-       sitrawebnew.dbo.Cidades AS CID ON DR.Cidade = CID.Cidade LEFT JOIN
-       sitrawebnew.dbo.Cidades AS CID2 ON DD.Cidade = CID.Cidade WHERE ((dh.OcorrenciaId IS NULL
-                                                                             AND coalesce(oc.MercIndeziada,0) != 1
-                                                                             AND coalesce(occ.MercIndeziada,0) != 1) or(dh.OcorrenciaId IS NOT null)
-                                                                       )
+   AND dh.OcorrenciaId != 0 
+  INNER JOIN SitraWebNew.dbo.Usuarios cadUser ON cadUser.UsuarioId = dh.UsuarioIdHistorico 
+  INNER JOIN DocRemetente dr ON dr.ClienteId = doc.RemetenteId AND dr.DocId = doc.DocId 
+  INNER JOIN DocDestinatario dd ON dd.ClienteId = doc.DestinatarioId AND dd.DocId = doc.DocId 
+  INNER JOIN DocTomador dt ON dt.ClienteId = doc.TomadorId AND dt.DocId = doc.DocId 
+  LEFT JOIN  Ocorrencias oc ON oc.OcorrenciaId = dh.OcorrenciaId AND oc.OcorrenciaTipo = 'E' AND dh.OcorrenciaCliente = 0 
+  LEFT JOIN  OcorClienteEntrega occ ON occ.OcorrenciaId = dh.OcorrenciaId AND occ.ClienteId = dt.ClienteId AND dh.OcorrenciaCliente = 1 
+  LEFT JOIN  sitrawebnew.dbo.cep AS CEP ON DR.CEP = CEP.CepId 
+  LEFT JOIN  sitrawebnew.dbo.cep AS CEP2 ON DD.CEP = CEP.CepId 
+  LEFT JOIN  sitrawebnew.dbo.Estados AS UF ON DR.UF = UF.UF 
+  LEFT JOIN  sitrawebnew.dbo.Estados AS UF2 ON DD.UF = UF2.UF 
+  INNER JOIN SitraWebnew.DBO.Cidades AS CIDX ON CIDX.EstadoId = UF.ESTADOID
+  LEFT join  sitrawebnew.dbo.Cidades  AS CID  ON  DR.Cidade COLLATE sql_latin1_general_cp1_ci_ai = CID.Cidade COLLATE sql_latin1_general_cp1_ci_ai AND DR.UF = UF.UF 
+  LEFT JOIN  sitrawebnew.dbo.Cidades AS CID2 ON DD.Cidade COLLATE sql_latin1_general_cp1_ci_ai = CID2.Cidade COLLATE sql_latin1_general_cp1_ci_ai AND DD.UF = UF2.UF 
+ WHERE((dh.OcorrenciaId IS NULL
+            AND coalesce(oc.MercIndeziada,0) != 1
+            AND coalesce(occ.MercIndeziada,0) != 1) or(dh.OcorrenciaId IS NOT null))
    AND dh.HistoricoId IN  (SELECT HistoricoId
                              FROM (SELECT doc.DocId
                                         , Max(dh.HistoricoId) HistoricoId
@@ -68,5 +64,7 @@ SELECT doc.DocId
                                      WHERE doc.StatusDocumentoId = 8
                                      GROUP BY doc.DocId
                                    ) DadosOcorrencias2
-                           )
+                           ) 
+
+
 
